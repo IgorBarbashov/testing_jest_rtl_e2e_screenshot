ebeeb207fea086572723360c08cfd0012d613607
Igor Barbashov <bis200705@gmail.com>
2024-04-06 18:08:08 +0300

    unit tests with spyOn function

--- jest-unit/src/square/square.js
+++ jest-unit/src/square/square.js
@@ -1,3 +1,9 @@
-const square = (n) => n * n;
+const square = (n) => {
+    if (n === 1) {
+        return n;
+    }
+
+    return Math.pow(n, 2);
+};
 
 module.exports = square;
--- jest-unit/src/square/square.test.js
+++ jest-unit/src/square/square.test.js
@@ -1,10 +1,22 @@
 const square = require('./square');
 
-describe('square function', () => {
-    it('should return original array of numbers', () => {
-        expect(square(2)).toBe(4);
-        expect(square(2)).toBeLessThan(7);
-        expect(square(2)).toBeGreaterThan(-1);
-        expect(square(2)).not.toBeUndefined();
+describe('Math.pow in the square function', () => {
+
+    it('should\'t be called with argument = 1 ', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(1);
+        expect(spyMathPow).not.toBeCalled();
+    });
+
+    it('should be called with argument != 1', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2);
+        expect(spyMathPow).toBeCalled();
+    });
+
+    it('should be called once with argument = 1', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2);
+        expect(spyMathPow).toBeCalledTimes(1);
     });
 });
