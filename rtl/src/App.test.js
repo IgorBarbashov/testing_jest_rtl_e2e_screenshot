6a079f8f7eed167e0f3cbb9789bdb0981ebe4554
Igor Barbashov <bis200705@gmail.com>
2024-04-07 18:10:11 +0300

    add negative test (queryByText)

--- rtl/src/App.test.js
+++ rtl/src/App.test.js
@@ -1,16 +1,29 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('App component', () => {
-    render(<App />);
-    const helloWorldElement = screen.getByText(/hello world/i);
-    const buttonElement = screen.getByRole('button');
-    const inputElement = screen.getByPlaceholderText(/enter value/i);
-    expect(helloWorldElement).toBeInTheDocument();
-    expect(buttonElement).toBeInTheDocument();
-    expect(inputElement).toBeInTheDocument();
+describe('App component', () => {
+    test('should render fields', () => {
+        render(<App />);
+        const helloWorldElement = screen.getByText(/hello world/i);
+        const buttonElement = screen.getByRole('button');
+        const inputElement = screen.getByPlaceholderText(/enter value/i);
+        expect(helloWorldElement).toBeInTheDocument();
+        expect(buttonElement).toBeInTheDocument();
+        expect(inputElement).toBeInTheDocument();
 
-    expect(inputElement).toMatchSnapshot();
+        screen.debug();
+    });
 
-    screen.debug();
+    test('should correctly render field input', () => {
+        render(<App />);
+        const inputElement = screen.getByPlaceholderText(/enter value/i);
+        expect(inputElement).toMatchSnapshot();
+    });
+
+    test('negative test', () => {
+        render(<App />);
+        const inputElement = screen.queryByText(/try to find me/i);
+        expect(inputElement).not.toBeInTheDocument();
+        expect(inputElement).toBeNull();
+    });
 });
