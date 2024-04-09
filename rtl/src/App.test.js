79d5afbc216327e50f5878d0ab26c66686642df6
Igor Barbashov <bis200705@gmail.com>
2024-04-09 14:11:12 +0300

    fix unit tests

--- rtl/src/App.test.js
+++ rtl/src/App.test.js
@@ -2,44 +2,47 @@ import {render, screen, fireEvent} from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import {MemoryRouter} from "react-router-dom";
 import App from './App';
+import {renderTestApp} from "./tests/helpers/renderTestApp";
+import Navbar from "./components/Navbar/Navbar";
+import React from "react";
 
 describe('App component', () => {
     test('should render fields', () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const testAppElement = screen.getByText(/test app/i);
-        const buttonElement = screen.getByRole('button');
+        const buttonElement = screen.getByTestId('toggle-btn');
         const inputElement = screen.getByPlaceholderText(/enter value/i);
         expect(testAppElement).toBeInTheDocument();
         expect(buttonElement).toBeInTheDocument();
         expect(inputElement).toBeInTheDocument();
 
-        // screen.debug();
+        screen.debug();
     });
 
     test('should correctly render field input', () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const inputElement = screen.getByPlaceholderText(/enter value/i);
         expect(inputElement).toMatchSnapshot();
     });
 
     test('negative test', () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const inputElement = screen.queryByText(/try to find me/i);
         expect(inputElement).not.toBeInTheDocument();
         expect(inputElement).toBeNull();
     });
 
     test('async test', async () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
-        // screen.debug();
+        renderTestApp(<App />);
+        screen.debug();
         const inputElement = await screen.findByText(/data/i);
         expect(inputElement).toBeInTheDocument();
         expect(inputElement).toHaveStyle({color: 'red;'});
-        // screen.debug();
+        screen.debug();
     });
 
     test('click event', async () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const btn = screen.getByTestId("toggle-btn");
         expect(screen.queryByTestId("toggle-elem")).toBeNull();
         fireEvent.click(btn);
@@ -49,7 +52,7 @@ describe('App component', () => {
     });
 
     test('change event', async () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const inputElement = screen.getByPlaceholderText(/enter value/i);
         expect(screen.queryByTestId("input-header")).toContainHTML('');
         expect(inputElement.value).toBe('');
@@ -62,7 +65,7 @@ describe('App component', () => {
     });
 
     test('userEvent tests', async () => {
-        render(<MemoryRouter><App /></MemoryRouter>);
+        renderTestApp(<App />);
         const inputElement = screen.getByPlaceholderText(/enter value/i);
         expect(screen.queryByTestId("input-header")).toContainHTML('');
         expect(inputElement.value).toBe('');
--- rtl/src/components/Navbar/Navbar.test.jsx
+++ rtl/src/components/Navbar/Navbar.test.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar";
-import {renderWithRouter} from "../../tests/helpers/renderWithRouter";
+import {renderTestApp} from "../../tests/helpers/renderTestApp";
 import {screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe('Navbar component', () => {
     test('should render navbar links and go through main-link', async () => {
-        renderWithRouter(<Navbar />, '/about');
+        renderTestApp(<Navbar />, {
+            initialRoute: '/about'
+        });
         userEvent.click(screen.getByTestId('main-link'));
         expect(screen.getByTestId('main-page')).toBeInTheDocument();
     });
 
     test('should render navbar links and go through about-link', async () => {
-        renderWithRouter(<Navbar />);
+        renderTestApp(<Navbar />);
         userEvent.click(screen.getByTestId('about-link'));
         expect(screen.getByTestId('about-page')).toBeInTheDocument();
     });
 
     test('should render navbar links and go through posts-link', async () => {
-        renderWithRouter(<Navbar />);
+        renderTestApp(<Navbar />);
         userEvent.click(screen.getByTestId('posts-link'));
         expect(screen.getByTestId('posts-page')).toBeInTheDocument();
     });
